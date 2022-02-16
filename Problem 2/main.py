6d08706555c0ed94e5836ea4a1870145452bdb0b
Hîrbu Andrei <andrei_hrb@icloud.com>
2022-02-16 13:28:01 +0200

    add second problem

---
+++ Problem 2/main.py
@@ -0,0 +1,18 @@
+def is_even(n):
+    return not n % 2
+
+
+if __name__ == '__main__':
+    _sum = 0
+
+    first = 1
+    second = 2
+
+    while second < 4000000:
+        if is_even(second):
+            _sum = _sum + second
+        aux = first
+        first = second
+        second = second + aux
+
+    print(_sum)
