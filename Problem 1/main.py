c97ea515a212ef0dbf98f43e1610621a6e551912
Hîrbu Andrei <andrei_hrb@icloud.com>
2022-02-16 13:23:45 +0200

    Create repo and add first problem

---
+++ Problem 1/main.py
@@ -0,0 +1,12 @@
+def multiple_of_3_or_5(n):
+    return n % 3 == 0 or n % 5 == 0
+
+
+if __name__ == '__main__':
+    _sum = 0
+
+    for number in range(1, 1000):
+        if multiple_of_3_or_5(number):
+            _sum = _sum + number
+
+    print(_sum)
